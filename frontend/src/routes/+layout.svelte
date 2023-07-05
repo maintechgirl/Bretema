3b4910bd00bf75c2886eeb38fa9e085081c96a66
factoria F5 <tatiyanakedrova94@gmail.com>
2023-07-05 11:52:04 +0200

    setup global layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<main>
+        <slot />
+</main>
+
+<style>
+        :global(body)  {
+        font-family: 'Inter', sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+       main{
+               width: 480px;
+               height: 800px;
+
+               background-image:  url(/images/fondo.png);
+               background-position: center;
+               background-size: cover;
+       }
+</style>
