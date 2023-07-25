5dec7e475b6eeac5bfa3627ba1937b5fe636f6ad
maintekgirl <tatiyanakedrova94@gmail.com>
2023-07-25 14:53:50 +0200

    (backend) Refactoring function addProduct in ProductController

--- backend/src/main/kotlin/com/bretema/backend/controllers/ProductController.kt
+++ backend/src/main/kotlin/com/bretema/backend/controllers/ProductController.kt
@@ -3,6 +3,7 @@ package com.bretema.backend.controllers
 import com.bretema.backend.domain.Product
 import com.bretema.backend.repositories.ProductRepository
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.server.ResponseStatusException
@@ -12,9 +13,9 @@ import kotlin.jvm.optionals.getOrNull
 
 @RestController
 @CrossOrigin
-class ProductController (private val productRepository: ProductRepository) {
+class ProductController(private val productRepository: ProductRepository) {
 
-    @GetMapping
+    @GetMapping("/")
     fun allproducts(@RequestParam search: String?): List<ProductDTO> {
         if (!search.isNullOrBlank()) {
             val filteredProducts = productRepository.findByNameContainingIgnoreCase(search)
@@ -35,30 +36,52 @@ class ProductController (private val productRepository: ProductRepository) {
     }
 
     @PostMapping("/")
-    fun addProduct(@RequestBody product: Product): Product? {
-        return productRepository.save(product)
+    fun addProduct(@RequestBody product: Product): ResponseEntity<Product> {
+        try {
+            val savedProduct = productRepository.save(product)
+            return ResponseEntity.ok(savedProduct)
+        } catch (ex: Exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+        }
     }
 
-    @PutMapping("/")
-    fun updateProductById(@RequestBody product: Product): Product? {
-        product.id?.let { productRepository.findById(it).orElseThrow { ProductNotFoundException() } }
-        return productRepository.save(product)
-    }
+    //@PostMapping("/{id}")
+    //fun updateProduct(
+    //    @PathVariable id: String,
+    //    @RequestBody updatedProduct: Product
+    //): ResponseEntity<Product> {
+    //    val product = productRepository.findById(UUID.fromString(id)).getOrNull()
+
+    //    if (product == null) {
+    //        return ResponseEntity.notFound().build()
+    //    }
+
+    //    product.name = updatedProduct.name
+    //    product.quantity = updatedProduct.quantity
+        // Update other properties as needed
+
+    //    val updatedProduct = productRepository.save(product)
+    //    return ResponseEntity.ok(updatedProduct)
+    //}
+
+
+    //@PutMapping("/")
+    //fun updateProductById(@RequestBody product: Product): Product? {
+    //    product.id.let { productRepository.findById(it).orElseThrow { ProductNotFoundException() } }
+    //   return productRepository.save(product)
+    // }
 
     @DeleteMapping("/{id}")
     fun deleteProductById(@PathVariable id: String): ProductDTO? {
         val product = productRepository
             .findById(UUID.fromString(id))
             .getOrNull()
-
         if (product == null) {
             throw ResponseStatusException(HttpStatus.NOT_FOUND)
         }
         productRepository.delete(product)
-
         return ProductDTO.from(product)
     }
-
 }
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "product not found")
